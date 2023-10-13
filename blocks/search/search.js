2d8f7d99e546245982197c4155828883d38d8e34
Raul Hudea <rhudea@adobe.com>
2023-10-13 11:56:40 +0300

    Add hero to LCP Add a11y to search button

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -8,7 +8,7 @@ export default async function decorate(block) {
     `
 <label for="js-search-anywhere" class="search-label"></label>
 <input type="text" id="js-search-anywhere" class="search-input" placeholder="Search Help Centre" tabindex="-1">
-<button type="button" class="search-button">
+<button type="button" tabindex="-1" aria-label="search" class="search-button">
     <span class="icon icon-search"></span>
 </button>
 <div class="search-info">
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,7 @@ import {
   loadCSS,
 } from './lib-franklin.js';
 
-const LCP_BLOCKS = []; // add your LCP blocks to the list
+const LCP_BLOCKS = ['hero']; // add your LCP blocks to the list
 
 /**
  * Builds hero block and prepends to main in a new section.
