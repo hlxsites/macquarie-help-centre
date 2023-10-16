88e76132f080ee12e3481613cb3b7062eee0982d
Raul Hudea <rhudea@adobe.com>
2023-10-16 22:43:26 +0300

    Delayed breadcrumbs

--- blocks/breadcrumbs/breadcrumbs.js
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -10,18 +10,27 @@ function getName(pageIndex, path) {
   return pg && pg.shorttitle;
 }
 
-export default async function decorate(block) {
+export async function loadBreadbcrumbs() {
+  const block = document.querySelector('.breadcrumbs');
+  if (!block) {
+    return;
+  }
   const breadcrumbs = [];
   const path = window.location.pathname;
   const pathSplit = path.split('/');
   const pageIndex = await ffetch('/query-index.json').all();
-  const urlForIndex = (index) => prependSlash(pathSplit.slice(1, index + 2).join('/'));
+  const urlForIndex = (index) => prependSlash(pathSplit.slice(1, index + 2)
+    .join('/'));
 
   breadcrumbs.push(
-    ...pathSplit.slice(1, -1).map((part, index) => {
-      const url = urlForIndex(index);
-      return { name: getName(pageIndex, url), url_path: url };
-    }),
+    ...pathSplit.slice(1, -1)
+      .map((part, index) => {
+        const url = urlForIndex(index);
+        return {
+          name: getName(pageIndex, url),
+          url_path: url,
+        };
+      }),
   );
 
   const ol = createTag('ol', {}, '');
@@ -33,5 +42,11 @@ export default async function decorate(block) {
       `<a itemprop="item" href='${crumb.url_path}'><span itemprop="name">${crumb.name}</span></a>`,
     ));
   ol.append(...liItems);
-  block.appendChild(ol);
+  block.replaceChildren(ol);
+}
+
+// eslint-disable-next-line no-empty-function
+export default async function decorate(block) {
+  const ol = createTag('ol', { class: 'breadcrumbs-hidden' }, '<li>/</li>');
+  block.append(ol);
 }
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import { loadBreadbcrumbs } from '../blocks/breadcrumbs/breadcrumbs.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+loadBreadbcrumbs();
