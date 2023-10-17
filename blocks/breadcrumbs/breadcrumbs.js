f6ef6147f696e057b4745530d655f092c383bf51
Raul Hudea <rhudea@adobe.com>
2023-10-17 14:48:21 +0300

    Add support for Breadcrumbs (#37)
    * Initial breadcrumbs block
    
    * Delayed breadcrumbs
    
    * Better hero styling
    
    * Fix breadcrumb style
    
    * Automatically add breadcrumbs
    
    * Breadcrumbs are added automatically

---
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,52 @@
+import ffetch from '../../scripts/ffetch.js';
+import { createTag } from '../../scripts/lib-franklin.js';
+
+function prependSlash(path) {
+  return path.startsWith('/') ? path : `/${path}`;
+}
+
+function getName(pageIndex, path) {
+  const pg = pageIndex.find((page) => page.path === path);
+  return pg && pg.shorttitle;
+}
+
+export async function loadBreadbcrumbs() {
+  const block = document.querySelector('.breadcrumbs');
+  if (!block) {
+    return;
+  }
+  const breadcrumbs = [];
+  const path = window.location.pathname;
+  const pathSplit = path.split('/');
+  const pageIndex = await ffetch('/query-index.json').all();
+  const urlForIndex = (index) => prependSlash(pathSplit.slice(1, index + 2)
+    .join('/'));
+
+  breadcrumbs.push(
+    ...pathSplit.slice(1, -1)
+      .map((part, index) => {
+        const url = urlForIndex(index);
+        return {
+          name: getName(pageIndex, url),
+          url_path: url,
+        };
+      }),
+  );
+
+  const ol = createTag('ol', {}, '');
+  const liItems = breadcrumbs
+    .filter((crumb) => crumb)
+    .map((crumb) => createTag(
+      'li',
+      {},
+      `<a itemprop="item" href='${crumb.url_path}'><span itemprop="name">${crumb.name}</span></a>`,
+    ));
+  ol.append(...liItems);
+  block.replaceChildren(ol);
+}
+
+// eslint-disable-next-line no-empty-function
+export default async function decorate(block) {
+  const ol = createTag('ol', { class: 'breadcrumbs-hidden' }, '<li>/</li><li>/</li>');
+  block.append(ol);
+}
--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -4,10 +4,16 @@ import {
   loadBlock,
 } from '../../scripts/lib-franklin.js';
 
+function hasBreadcrumbs() {
+  const parts = window.location.pathname.split('/').filter((p) => p);
+  return parts.length > 1;
+}
+
 export default async function decorate(block) {
   const title = block.querySelector('h1');
   const info = block.querySelector('h5');
   const search = block.querySelector('.search');
+  const breadcrumbs = hasBreadcrumbs() && createTag('div', { class: 'breadcrumbs' }, '');
   const picture = block.querySelector('picture');
   if (picture) {
     picture.parentElement.classList.add('hero-picture');
@@ -17,10 +23,14 @@ export default async function decorate(block) {
 
   const content = createTag('div', { class: 'hero-content' }, [info, title].filter((e) => e));
   const box = createTag('div', { class: 'hero-box' }, [content, search].filter((e) => e));
-  const containerOuter = createTag('div', { class: 'hero-outer' }, [box]);
+  const containerOuter = createTag('div', { class: 'hero-outer' }, [breadcrumbs, box].filter((e) => e));
   block.append(containerOuter);
   if (search) {
     decorateBlock(search);
     await loadBlock(search);
+  }
+  if (breadcrumbs) {
+    decorateBlock(breadcrumbs);
+    await loadBlock(breadcrumbs);
   }
 }
