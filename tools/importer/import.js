423510d15632c107ad2ac0b097937dac1946f12e
David Catalan <catalan@adobe.com>
2023-10-19 15:57:43 +0200

    fix: force remove noscript tags after import transformation

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -318,6 +318,7 @@ export default {
     // use helper method to remove header, footer, etc.
     WebImporter.DOMUtils.remove(main, [
       'iframe',
+      'noscript',
     ]);
 
     return main;
