0c8d8c71319f6159d0640a3d0027782ffd36f8d4
David Catalan <catalan@adobe.com>
2023-10-25 16:08:57 +0200

    feat: structure reporting

--- tools/analysis/process-dataset-hc-anchor-links.js
+++ tools/analysis/process-dataset-hc-anchor-links.js
@@ -8,6 +8,10 @@ const JSON_FOLDER = `storage-${DATASET}/datasets/${DATASET}`;
 (async() => {
   const jsonsInDir = fs.readdirSync(JSON_FOLDER).filter(file => path.extname(file) === '.json');
 
+  const anchorLinks = [];
+  const htmlTables = [];
+  const listsWithStartAttr = [];
+
   for (let i = 0; i < jsonsInDir.length; i++) {
     const file = jsonsInDir[i];
     try {
@@ -15,14 +19,67 @@ const JSON_FOLDER = `storage-${DATASET}/datasets/${DATASET}`;
       const json = JSON.parse(fileData.toString());
       
       /**
-       * process blocks data
+       * process anchor links data
        */
 
       if (json.anchorLinks && json.anchorLinks.length > 0) {
-        console.log(json.url);
+        anchorLinks.push({
+          file,
+          url: json.url,
+          anchorLinks: json.anchorLinks,
+        });
       }
+      
+      /**
+       * process html table data
+       */
+
+      if (json.outerHTML?.includes('<table')) {
+        const n = json.outerHTML.split('<table').length - 1;
+        htmlTables.push({
+          file,
+          url: json.url,
+          n,
+        });
+      }
+      
+      /**
+       * process lists using "start" attribute
+       */
+
+      if (json.outerHTML?.includes('start=')) {
+        const n = json.outerHTML.split('start=').length - 1;
+        listsWithStartAttr.push({
+          file,
+          url: json.url,
+          n,
+        });
+      }
+
     } catch(e) {
       console.log('error', e);
     }
   }
+
+  // report
+
+  // anchor links
+  console.log('Pages with anchor links:', anchorLinks.length);
+  anchorLinks.forEach(({ url, file, anchorLinks }) => {
+    console.log(`${url} (${file}): ${anchorLinks.length}`);
+  });
+
+  // html tables
+  console.log('');
+  console.log('Pages with html tables:', htmlTables.length);
+  htmlTables.forEach(({ url, file, n }) => {
+    console.log(`${url} (${file}): ${n}`);
+  });
+
+  // lists with start attr
+  console.log('');
+  console.log('Pages with lists with start attr:', listsWithStartAttr.length);
+  listsWithStartAttr.forEach(({ url, file, n }) => {
+    console.log(`${url} (${file}): ${n}`);
+  });
 })();
